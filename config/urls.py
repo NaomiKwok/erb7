5ebeef82d5f091d8a7a92e51fec686a05a9191f9
Naomi <naomikwokster@gmail.com>
2025-10-16 11:34:36 +0800

    Step 20 : final finished

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',include("pages.urls",namespace="pages")),
-    path('listings/',include("listings.urls",namespace="listings")),    
+    path('listings/',include("listings.urls",namespace="Listings")),    
     path('contacts/',include("contacts.urls",namespace="contacts")),
     path('accounts/',include("accounts.urls",namespace="accounts")),
     path('admin/', admin.site.urls),
